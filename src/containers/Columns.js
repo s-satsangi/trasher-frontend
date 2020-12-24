ba013eb2929bfb11e3ce87391441b8fa70d4a56c
jrshort89 <jrshort89@gmail.com>
2020-12-23 21:32:36 -0500

    design

--- src/containers/Columns.js
+++ src/containers/Columns.js
@@ -3,6 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import PostLayout from "./PostLayout";
+import InfiniteScroll from "react-infinite-scroller";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -19,12 +20,29 @@ export default function CenteredGrid() {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:3000/posts")
+    setPosts(getPosts());
+  }, []);
+
+  const getPosts = () => {
+    let postsRequest;
+    const postsLength = posts ? posts.length : 0;
+    fetch(
+      `http://localhost:3000/posts/${postsLength}`,
+      {
+        // client_id: api.client_id,
+        linked_partitioning: 1,
+        page_size: 10,
+      },
+      {
+        cache: true,
+      }
+    )
       .then((data) => data.json())
       .then((json) => {
-        const posts = json.map((post) => {
+        postsRequest = json.map((post) => {
           return (
             <PostLayout
+              key={post.id}
               image={post.image}
               location={post.locaiton}
               text={post.text}
@@ -32,9 +50,9 @@ export default function CenteredGrid() {
             />
           );
         });
-        setPosts(posts);
+        setPosts(postsRequest);
       });
-  }, []);
+  };
 
   const classes = useStyles();
   let arr = ["Notifications", "Settings", "Likes", "Som other shit", 5];
@@ -42,20 +60,37 @@ export default function CenteredGrid() {
   return (
     <div className={classes.root}>
       <Grid container spacing={2} direction="column">
-        <Grid item xs={12} container>
+        <Grid item xs={12} container className="menu">
           <Grid xs={2}>Jake</Grid>
           <Grid xs={8} />
           <Grid xs={2}>Short</Grid>
         </Grid>
         <Grid xs={12} container>
           <Grid xs={3}>
-            <Paper className={classes.paper}>{arr}</Paper>
+            <div className="menu">
+              <Paper className={classes.paper}>{arr}</Paper>
+            </div>
           </Grid>
-          <Grid container xs={6} className={"flex-section"}>
-            <Grid className="flex-col-scroll">{posts}</Grid>
+          <Grid container xs={7}>
+            <Grid container className="flex-section">
+              <InfiniteScroll
+                pageStart={0}
+                loadMore={getPosts}
+                hasMore={true || false}
+                loader={
+                  <div className="loader" key={0}>
+                    Loading ...
+                  </div>
+                }
+              >
+                {posts}
+              </InfiniteScroll>
+            </Grid>
           </Grid>
-          <Grid item xs={3}>
-            <Paper className={classes.paper}>xs=6</Paper>
+          <Grid item xs={2}>
+            <div className="menu">
+              <Paper className={classes.paper}>xs=6</Paper>
+            </div>
           </Grid>
         </Grid>
       </Grid>
