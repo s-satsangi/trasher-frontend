4a4e492399284b946d77d34f6927fde207b2ba17
s-satsangi <s.satsangi@gmail.com>
2020-12-29 13:23:34 -0500

    login works, but maybe not routes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import Columns from "./containers/Columns";
 
 import { Container, Grid } from "@material-ui/core";
+import Login from "./components/login"
 
 function App() {
   return (
     <Container>
       <Columns />
+      <Login />
     </Container>
   );
 }
